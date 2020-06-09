43ed375b4921021a601663c89039dd44c9ca2985
Palak Jadav <palakjadav.1996@gmail.com>
2020-06-09 09:34:11 +0530

    update database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(SubCategorySeeder::class);
     }
